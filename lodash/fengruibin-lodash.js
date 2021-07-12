d4c76fc41914797476c898488cc66130cad61d6b
FengRuiBin <1332694317@qq.com>
2021-07-12 15:25:15 +0800

    7.12

--- lodash/fengruibin-lodash.js
+++ lodash/fengruibin-lodash.js
@@ -1,2 +1,13 @@
-<script>
-</script>
+var FengRuiBin = function () {
+
+  function chunk(array, size) {
+    let result = []
+    if (!array || size < 1) {
+      return []
+    }
+    for (let i = 0; i < array.length; i += size) {
+      res.push(array.slice(i, i + size))
+    }
+    return result
+  }
+}
