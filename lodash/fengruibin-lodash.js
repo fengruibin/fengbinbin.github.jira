e9e5e42ced978949d237ef9889654663cddf0e09
fengruibin <1332694317@qq.com>
2021-08-09 19:27:22 +0800

    8.9

--- lodash/fengruibin-lodash.js
+++ lodash/fengruibin-lodash.js
@@ -1,9 +1,9 @@
 var fengruibin = function () {
 
-  function chunk(array, size = 1) {
-    var result = []
+  function chunk(array, [size = 1]) {
+    let result = []
     if (!array || size < 1) return result
-    for (var i = 0; i < array.length; i+=size) {
+    for (let i = 0; i < array.length; i += size) {
       result.push(array.slice(i, i + size))
     }
     return result
@@ -11,14 +11,36 @@ var fengruibin = function () {
 
 
   function compact(array) {
-    
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
   }
+
+
+  function concat(array, ...values) {
+    var result = []
+    if (!array && !values) return result
+    if (array) return array
+    if (values) {
+      for (let i = 0; i < values.length; i++) {
+        result.push(values[i])
+      }
+      return j
+    } else {
+      for (let i = 0; i < values.length; i++) {
+        return array.push(values[i])
+      }
+    }
+  }
+
 
   return {
     chunk,
     compact,
+    concat,
   }
 }()
-
-
-
